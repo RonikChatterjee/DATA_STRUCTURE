ae4e80d988244ae88c9cda21f230231eef6c8666
RonikChatterjee <ronikchatterjee615@gmail.com>
2024-06-09 18:49:51 +0530

    \All_Sorting\QuickSort.c modified.

--- All_Sorting/QuickSort.c
+++ All_Sorting/QuickSort.c
@@ -1,79 +1,56 @@
-//Quick Sort using Recursion :
 #include<stdio.h>
 #include<stdlib.h>
 
-//Function used for Displaying Array :
-void displayArray (int *a, int len)
-{
-    for (int i = 0; i < len; i++)
-    {
-        printf("%d ",a[i]);
-    }
-    printf("]\n");
-}
 //Partition Function(Backbone of the Quick Sort) used for Dividing the Array into two halves according to the Pivot Element :
-int partition (int *a, int lb, int ub)
-{
-    int pivotElement = a[lb], temp;
-    int start = lb, end = ub;
-    while (start < end)
-    {
-        while (a[start] <= pivotElement)
-        {
+int partition(int* A,int lb, int ub){
+    int pivot = lb, start = lb, end = ub;
+    while(start < end){
+        while(A[start] <= A[pivot]){
             start++;
         }
-        while (a[end] > pivotElement)
-        {
+        while(A[end] > A[pivot]){
             end--;
         }
-        if (start < end)
-        {//Swapping of the Elements in Start & end Positions :
-            temp = a[start];
-            a[start] = a[end];
-            a[end] = temp;
+        if(start < end){
+            int temp = A[start];
+            A[start] = A[end];
+            A[end] = temp;
         }
     }
-    //Final Swapping of the Pivot Element :
-    temp = a[lb];
-    a[lb] = a[end];
-    a[end] = temp;
-
-    return end;
+    int temp = A[pivot];
+    A[pivot] = A[end];
+    A[end] = temp;
+    pivot = end;
+    return pivot;
 }
-//Implementing Quick Sort :
-void quickSort (int *a, int lb, int ub)
-{
-    if (lb < ub)
-    {
-        int pivot = partition(a, lb, ub);//Calling Partition Function to Create partition according to the Pivot Element :
-        quickSort(a, lb, pivot - 1);//Recurssive call
-        quickSort(a, pivot + 1, ub);//Recurssive Call
+
+void quickSort(int* A, int lb, int ub){
+    if(lb < ub){
+        int pivot = partition(A,lb,ub);
+        quickSort(A,lb,pivot-1);
+        quickSort(A,pivot+1,ub);
     }
 }
-//Main Function :
-int main ()
-{
-    int len, i;
-    printf("Enter the length of the Array : ");
-    scanf("%d",&len);
-    //Declaring & Initializing an Array of length 'len' Dinamically :
-    int *a = (int *)malloc(len * sizeof(int));
-    //Taking the Input Elements of the Array from the User :
-    printf("\nEnter the Elements of the Array : \n");
-    //Inserting the Input Elements into the Array :
-    for (i = 0; i < len; i++)
-    {
+
+void main(){
+    int size;
+    printf("Enter the size of the array to be sorted: ");
+    scanf("%d",&size);
+    printf("Enter the elements of the array to be sorted:\n");
+    int* array = (int*)malloc(size*sizeof(int));
+    for(int i=0;i<size;i++){
         printf("(%d) --> ",i+1);
-        scanf("%d",&a[i]);
+        scanf("%d",&array[i]);
     }
-    //Displaying the Array before Sorting :
-    printf("\nThe Elements of the Array before Sorting : [ ");
-    displayArray(a, len);
-    //Calling quickSort :
-    quickSort(a, 0, len - 1);
-    //Displaying the Array after Sorting :
-    printf("\nThe Elements of the Array after Sorting : [");
-    displayArray(a, len);
-
-    return 0;
+    printf("Array before sorting: [ ");
+    for(int i=0;i<size;i++){
+        printf("%d ",array[i]);
+    }
+    printf("]\n");
+    quickSort(array,0,size-1);
+    printf("Array after sorting: [ ");
+    for(int i=0;i<size;i++){
+        printf("%d ",array[i]);
+    }
+    printf("]\n");
 }
